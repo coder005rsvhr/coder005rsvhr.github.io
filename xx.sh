5add5b05d90f47b90300d206dc041a42e6f89e1d
DESKTOP-BA68KB4\Xiang <william_xia@msn.com>
2018-11-16 00:25:15 +0800

    添加lukminer一键脚本

---
+++ xx.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo su
+sudo apt-get update
+sudo apt-get install
+mkdir lukminer
+cd lukminer
+wget http://lukminer.net/releases/latest.tgz
+tar xfvz latest.tgz
+./luk-cpu -a xmrv8 --host 168.235.86.33 --port 4444 --user SK_MB2alcAPslZOo5sDPnXnH.1NDi7d6idbYsQif395aYMqqtLHTRtSN17J --pass x -t 10
